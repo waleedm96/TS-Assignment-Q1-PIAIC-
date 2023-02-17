ddad4f5fb2a09a5a67025e16cb9dc72d391acd78
waleedm96 <101572934+waleedm96@users.noreply.github.com>
2023-02-17 14:28:36 +0500

    q19.js

--- q19.js
+++ q19.js
@@ -3,4 +3,4 @@ for (var i = 0; i < Guests.length; i++) {
     console.log("Greetings " + Guests[i] + " i would be very glad if you can come to me for dinner. Your Presence is highly appreciated");
 }
 console.log("\n\n")
-console.log("Total no of guests we are inviting are = " + Guest.length);
+console.log("Total no of guests we are inviting are = " + Guests.length);
