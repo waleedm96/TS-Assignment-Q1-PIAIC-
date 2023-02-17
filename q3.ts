86922f13d2a949ca35741aff34686fd27e4f2fab
waleedm96 <101572934+waleedm96@users.noreply.github.com>
2023-02-17 13:59:49 +0500

    q3.ts

--- q3.ts
+++ q3.ts
@@ -1,5 +1,5 @@
 let isName="waleed";
-console.log(aName.toUpperCase());
-console.log(aName.toLowerCase());
-let tCase=aName.replace(/(^|\s)\S/g,function(title){return title.toUpperCase()});
-console.log(titleCase);
+console.log(isName.toUpperCase());
+console.log(isName.toLowerCase());
+let tCase=isName.replace(/(^|\s)\S/g,function(title){return title.toUpperCase()});
+console.log(tCase);
