2963f8499d6c9c81a00a47be4ac0f47c0f0cbfb8
waleedm96 <101572934+waleedm96@users.noreply.github.com>
2023-02-17 14:11:01 +0500

    q12.js

--- q12.js
+++ q12.js
@@ -1,4 +1,4 @@
 var Arr = ["Waleed", "Shahmir", "Usama", "Shahzaib"];
-for (var i = 0; i < Ar.length; i++) {
-    console.log("Greetings " + Ar[i] + " what would you like to eat");
+for (var i = 0; i < Arr.length; i++) {
+    console.log("Greetings " + Arr[i] + " what would you like to eat");
 }
