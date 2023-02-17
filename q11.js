4b71bc0882c98955504b98e026396ea15ff6d231
waleedm96 <101572934+waleedm96@users.noreply.github.com>
2023-02-17 14:09:43 +0500

    q11.js

--- q11.js
+++ q11.js
@@ -1,5 +1,5 @@
 var myAr = ["Waleed", "Shahmir", "Usama", "Shahzaib"];
-console.log(myA[0]);
-console.log(myA[1]);
-console.log(myA[2]);
-console.log(myA[3]);
+console.log(myAr[0]);
+console.log(myAr[1]);
+console.log(myAr[2]);
+console.log(myAr[3]);
