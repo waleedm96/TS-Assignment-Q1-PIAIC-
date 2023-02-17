05aa11f6fe8422a3dca187a73c548edcf3299b41
waleedm96 <101572934+waleedm96@users.noreply.github.com>
2023-02-17 14:02:27 +0500

    q5.js

--- q5.js
+++ q5.js
@@ -1,3 +1,3 @@
 var famous_person = "John Lennon";
-var quote = person + ' once said, "Life is what happens when you are busy making other plans"';
+var quote = famous_person + ' once said, "Life is what happens when you are busy making other plans"';
 console.log(quote);
